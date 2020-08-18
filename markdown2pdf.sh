d76dd8431f97e5306b8bfbadd065ae117035764b
Philipp Bielefeldt <bielefeldt@uni-bonn.de>
2020-08-18 13:45:27 +0200

    markdown converter
    just so I don't forget my favourite options …

---
+++ markdown2pdf.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# transfer all .md files to pdf files using pandoc and XETEX
+
+
+#for i in *.md; do
+#	echo -e "### $i ###"
+#	pandoc "$i"  --standalone --from markdown --pdf-engine=xelatex -o "$i".pdf;
+#done
+
+pandoc "$1"  --standalone --from markdown --pdf-engine=xelatex \
+-V geometry:a4paper \
+-V colorlinks \
+-V urlcolor=NavyBlue \
+-V geometry:"top=2.5cm, bottom=1.5cm, left=3cm, right=2cm" \
+-V mainfont="Liberation Serif" \
+--highlight-style tango \
+-o "$1".pdf;
