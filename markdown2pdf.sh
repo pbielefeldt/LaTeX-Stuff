23d0a5df7e118ce5ff58568d8b8344c0c9d92115
Philipp Bielefeldt <p.bielefeldt@secure.mailbox.org>
2021-09-16 10:45:20 +0200

    add emoji support
    🥰

--- markdown2pdf.sh
+++ markdown2pdf.sh
@@ -7,11 +7,12 @@
 #	pandoc "$i"  --standalone --from markdown --pdf-engine=xelatex -o "$i".pdf;
 #done
 
-pandoc "$1"  --standalone --from markdown --pdf-engine=xelatex \
+pandoc "$1"  --standalone --from markdown+emoji --pdf-engine=xelatex \
 -V geometry:a4paper \
 -V colorlinks \
 -V urlcolor=NavyBlue \
 -V geometry:"top=2.5cm, bottom=1.5cm, left=3cm, right=2cm" \
 -V mainfont="Liberation Serif" \
+-V sansfont="Liberation Sans" \
 --highlight-style tango \
 -o "$1".pdf;
