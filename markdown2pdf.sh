b5088fc38b1003dfc0e04eccaf010b8f0dd2bafe
Philipp Bielefeldt <p.bielefeldt@secure.mailbox.org>
2022-01-28 21:43:34 +0100

    get name correct
    don't write XYZ.md.pdf, but only XYZ.pdf

--- markdown2pdf.sh
+++ markdown2pdf.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
-# transfer all .md files to pdf files using pandoc and XETEX
-
-
-#for i in *.md; do
-#	echo -e "### $i ###"
-#	pandoc "$i"  --standalone --from markdown --pdf-engine=xelatex -o "$i".pdf;
-#done
-
+# transfer markdown files to pdf files using pandoc and XETEX
+ 
+# get base name (including path)
+fn_0="$1"
+fn=${fn_0%.*}
+ 
 pandoc "$1"  --standalone --from markdown+emoji --pdf-engine=xelatex \
--V geometry:a4paper \
--V colorlinks \
--V urlcolor=NavyBlue \
--V geometry:"top=2.5cm, bottom=1.5cm, left=3cm, right=2cm" \
--V mainfont="Liberation Serif" \
--V sansfont="Liberation Sans" \
---highlight-style tango \
--o "$1".pdf;
+  -V geometry:a4paper \
+  -V colorlinks \
+  -V urlcolor=NavyBlue \
+  -V geometry:"top=2.5cm, bottom=1.5cm, left=3cm, right=2cm" \
+  -V mainfont="Liberation Serif" \
+  -V sansfont="Liberation Sans" \
+  --highlight-style tango \
+  -o ${fn}.pdf;
+ 
+echo "written ${fn}.pdf"
